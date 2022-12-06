5e22460ed3166eab555610d1b1cd973d1a608d6a
Matthias.Renaud <renaud4ma@cl2n012.legi.grenoble-inp.fr>
2022-12-06 18:53:56 +0100

    add Zacky HindranceModel class, correspond to Richardson and Zacky hindrance model

---
+++ src/settlingModels/HindranceModel/Zacky/Zacky.C
@@ -0,0 +1,58 @@
+/*---------------------------------------------------------------------------*\
+Copyright (C) 2022 Matthias Renaud, Cyrille Bonamy, Julien Chauchat
+                   and contributors
+
+License
+    This file is part of ScourFOAM.
+
+    ScourFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ScourFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ScourFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+#include "Zacky.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace settlingModels
+{
+    defineTypeNameAndDebug(Zacky, 0);
+    addToRunTimeSelectionTable(HindranceModel, Zacky, dictionary);
+}
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::settlingModels::Zacky::Zacky(const dictionary& dict)
+:
+    HindranceModel(dict)
+{
+}
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::settlingModels::Zacky::~Zacky()
+{}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::volScalarField> Foam::settlingModels::Zacky::hindrance
+(
+    const volScalarField& C
+) const
+{
+    scalar nExponent(dict_.get<scalar>("n"));
+    return pow((1-C),nExponent);
+}
