3e549561414c2fd5e36abdcdf36d9aeb176fc8fb
Matthias.Renaud <renaud4ma@cl7n001.legi.grenoble-inp.fr>
2023-01-05 16:54:23 +0100

    add data from Pham Van Bang experiment and python script to read them

--- tutorials/PY/plot_tuto1DSedim.py
+++ tutorials/PY/plot_tuto1DSedim.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+path_data = '../DATA/PhamVanBang2006/'
+
 def read_exp(path_file) :
     time_list = []
     z_dict = {}
@@ -18,16 +20,17 @@ def read_exp(path_file) :
             c_dict.update({t : c})
     return (time_list, z_dict, c_dict)
 
-time_list, z_dict, c_dict = read_exp('./concentration.txt')
-print(time_list)
+time_list, z_dict, c_dict = read_exp(path_data+'concentration.txt')
+print('times at which experimental data are available (in seconds) :')
+print(' ; '.join([str(t) for t in time_list]))
 
-t_int, z_int1, z_int2 = np.loadtxt('./interfaces.txt',unpack=True)
+t_int, z_int1, z_int2 = np.loadtxt(path_data+'./interfaces.txt',unpack=True)
 
 fig = plt.figure(figsize=(9,6))
 gs = fig.add_gridspec(1,2)
 
 ax0 = fig.add_subplot(gs[0,0])
-ax0.plot(c_dict[120],z_dict[120],ls='--')
+ax0.plot(c_dict[1200],z_dict[1200],ls='--')
 
 ax1 = fig.add_subplot(gs[0,1])
 ax1.plot(t_int,z_int1,marker='o',ls='None')
