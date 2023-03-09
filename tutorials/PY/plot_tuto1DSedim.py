6046e1d692cd003590ae333ec8c9083eb195501e
Matthias.Renaud <renaud4ma@cl2n012.legi.grenoble-inp.fr>
2023-03-09 17:04:26 +0100

    change divergence scheme in tuto 1DSedim and calibrate n coefficient to match experiment

--- tutorials/PY/plot_tuto1DSedim.py
+++ tutorials/PY/plot_tuto1DSedim.py
@@ -2,11 +2,12 @@ import numpy as np
 from fluidfoam import readof as rdf
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
 
 path_data = "../DATA/PhamVanBang2006/"
 path_tuto = "../suspensionFoam/laminar/1DSedim/"
 
-t_plot_list = [180, 600, 900, 1200]
+t_plot_list = [180, 540, 1140, 1500]
 # possible choices are :
 # 0.0 ; 60.0 ; 120.0 ; 180.0 ; 240.0 ; 300.0 ; 360.0 ; 420.0 ; 480.0 ; 540.0
 # 600.0 ; 660.0 ; 720.0 ; 780.0 ; 840.0 ; 900.0 ; 960.0 ; 1020.0 ; 1080.0
@@ -21,7 +22,7 @@ Cnum = []
 for t in t_plot_list:
     Cnum.append(rdf.readscalar(path_tuto, time_name=str(t), name="C"))
 
-# READ EXPERIMENTAL RESULTS #
+# --- READ EXPERIMENTAL RESULTS --- #
 
 
 def read_exp(path_file):
@@ -46,15 +47,67 @@ def read_exp(path_file):
 time_list, z_dict, c_dict = read_exp(path_data + "concentration.txt")
 print("times at which experimental data are available (in seconds) :")
 print(" ; ".join([str(t) for t in time_list]))
+z_array = z_dict[0]  # possible because all z array are identical
 
 # load experimental results, interface position
 t_int, y_int_sup, y_int_inf = np.loadtxt(
     path_data + "./interfaces.txt", unpack=True)
 
+# --- CORRECT TIME EXPERIMENT --- #
+
+
+t_end = 1040  # time at which interfaces join
+
+t_fit = []  # time liste for linear fit
+for t in t_int:
+    if t < t_end:
+        t_fit.append(t)
+t_fit = np.array(t_fit)
+n_elements = len(t_fit)
+y_new_inf = y_int_inf[:n_elements]
+
+# linear regression coefficients
+slope_fit = (np.mean(t_fit * y_new_inf)
+             - np.mean(t_fit)
+             * np.mean(y_new_inf)) / np.var(t_fit)
+
+y0_fit = np.mean(y_new_inf) - slope_fit * np.mean(t_fit)
+
+t0_true = (-0.1 - y0_fit)/slope_fit
+print("real start time of experiment : ", round(t0_true, 3), " s")
+
+# correct t_int
+t_int = t_int - t0_true
+
+
+def initial_Cexp(Z):
+    """initial volumic concentration profile"""
+    C_profile = 0.5 * 0.5 * (1 + np.tanh((Z - 0.054) / (0.049 - Z)*10))
+    return np.where(Z > 0.049, C_profile, 0.5)
+
+
+def interp_Cexp(t_val):
+    """Get value of c at time t_val through interpolation"""
+    if t_val < 0:
+        t1, t2 = t0_true, time_list[0]
+        c1, c2 = initial_Cexp(z_dict[t2] + 0.1), c_dict[t2]
+    else:
+        for i in range(len(time_list)):
+            if time_list[i+1] > t_val:
+                break
+        t1, t2 = time_list[i], time_list[i+1]
+        c1, c2 = c_dict[t1], c_dict[t2]
+    dt1, dt2 = t_val - t1, t2 - t_val
+    dt = dt1 + dt2
+    c_val = ((dt - dt1) * c1 + (dt - dt2) * c2) / dt
+    return c_val
+
+
 # --- FIND INTERFACE POSITION --- #
 
 
 def find_pos_inter_inf(Carr, Yarr, cmax=0.6, tol=0.05):
+    """Return position of lower interface"""
     c_inter = cmax * (1 - tol)
     for c, y in zip(Carr, Yarr):
         if c < c_inter:
@@ -64,6 +117,7 @@ def find_pos_inter_inf(Carr, Yarr, cmax=0.6, tol=0.05):
 
 
 def find_pos_inter_sup(Carr, Yarr, cmax=0.6, tol=0.05):
+    """Return position of upper interface"""
     c_inter = cmax * tol
     for c, y in zip(Carr, Yarr):
         if c < c_inter:
@@ -71,11 +125,6 @@ def find_pos_inter_sup(Carr, Yarr, cmax=0.6, tol=0.05):
     print("no interface found")
     return None
 
-
-# def fin_pos_inter_sup(Carr, Yarr, cmax=0.6, tol=0.02) :
-#    c_inter = cmax*tol
-#    for i in range(len(time_list)-1,-1,-1) :
-#        if Carr[i]
 
 Yinter_inf = np.zeros(len(time_list))
 Yinter_sup = np.zeros(len(time_list))
@@ -85,7 +134,8 @@ for i, t in enumerate(time_list):
     Yinter_inf[i] = find_pos_inter_inf(Ctemp, Ynum)
     Yinter_sup[i] = find_pos_inter_sup(Ctemp, Ynum)
 
-# PLOT RESULTS #
+# --- PLOT RESULTS --- #
+
 
 nplot = len(Cnum)
 nlines = 1 + (nplot - 1) // 2
@@ -101,9 +151,13 @@ for i, t in enumerate(t_plot_list):
     i_line, i_col = divmod(i, 2)
     axs.append(fig1.add_subplot(gs[i_line, i_col]))
     axs[i].plot(
-        c_dict[t], z_dict[t] * 100, ls="--", c=col_exp, label="experiment")
-    axs[i].plot(Cnum[i], Ynum * 100, lw=2, c=col_num, label="numerical")
+        interp_Cexp(t + t0_true), z_array * 100 + 10,
+        ls="--", lw=2, c=col_exp, label="experiment")
+    axs[i].plot(
+        Cnum[i], Ynum * 100 +10, lw=2, c=col_num, label="OpenFoam")
+
     axs[i].set_title(f"t = {t} s")
+    axs[i].set_ylim(0, 6)
     # add y label only if plot is on left side
     if i_col == 0:
         axs[i].set_ylabel("y [cm]")
@@ -116,10 +170,14 @@ fig2 = plt.figure(figsize=(6, 4))
 col_up, col_lo = "#1984c5", "#a57c1b"
 
 ax = fig2.add_subplot()
-ax.plot(t_int, y_int_sup * 100, marker="o", ls="None", c=col_up)
-ax.plot(t_int, y_int_inf * 100, marker="o", ls="None", c=col_lo)
-ax.plot(time_list, Yinter_sup * 100, c=col_up, lw=2)
-ax.plot(time_list, Yinter_inf * 100, c=col_lo, lw=2)
+ax.plot(t_int, y_int_sup * 100 + 10,
+        marker="o", ls="None", c=col_up)
+ax.plot(t_int, y_int_inf * 100 + 10,
+        marker="o", ls="None", c=col_lo)
+ax.plot(time_list, Yinter_sup * 100 + 10,
+        c=col_up, lw=2)
+ax.plot(time_list, Yinter_inf * 100 + 10,
+        c=col_lo, lw=2)
 
 ax.set_xlabel("t [s]")
 ax.set_ylabel("z [cm]")
@@ -127,6 +185,9 @@ ax.set_ylabel("z [cm]")
 legend_elements = [
     Patch(facecolor=col_up, label="upper interface"),
     Patch(facecolor=col_lo, label="lower interface"),
+    Line2D([0], [0], marker="o", ls="None",
+           color="black", label="experimental"),
+    Line2D([0], [0], color="black", label="numerical")
 ]
 
 ax.legend(
