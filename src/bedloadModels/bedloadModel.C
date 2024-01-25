47775ba8b2303f14d530ad0c4c1cd2b9a4666de8
Matthias.Renaud <renaud4ma@cl1f001.legi.grenoble-inp.fr>
2024-01-25 14:54:30 +0100

    new mean of instantiating bedloadModel

--- src/bedloadModels/bedloadModel.C
+++ src/bedloadModels/bedloadModel.C
@@ -20,22 +20,108 @@ License
 
 \*---------------------------------------------------------------------------*/
 
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
 #include "bedloadModel.H"
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::bedloadModel::bedloadModel()
+:
+    bedloadModel("Meyer-Peter")
+{}
 
 Foam::bedloadModel::bedloadModel
 (
     const word& modelName
 )
+:
+    critShields_(dimless, 0.047)
+{
+    modelName_ = modelName;
+    setCoefs(modelName_);
+}
+
+Foam::bedloadModel::bedloadModel
+(
+    const word& modelName,
+    const dimensionedScalar critShields
+)
+:
+    bedloadModel(modelName)
+{
+    critShields_ = critShields;
+}
+
+Foam::bedloadModel::bedloadModel
+(
+    scalar alpha,
+    scalar aExp,
+    scalar bExp,
+    const dimensionedScalar& critShields
+)
+:
+    bedloadModel()
+{
+    modelName_ = "custom";
+    critShields_ = critShields;
+    setCoefs(alpha, aExp, bExp);
+}
+
+Foam::bedloadModel::bedloadModel(const dictionary& dict)
+:
+    bedloadModel()
+{
+    if (dict.found("bedloadFormula"))
+    {
+        word name(dict.lookup("bedloadFormula"));
+        modelName_ = name;
+
+        if (name=="custom")
+        {
+            scalar alpha = readScalar(dict.lookup("alpha"));
+            scalar aExp = readScalar(dict.lookup("aExp"));
+            scalar bExp = readScalar(dict.lookup("bExp"));
+            setCoefs(alpha, aExp, bExp);
+        }
+        else
+        {
+            setCoefs(name);
+        }
+    }
+
+    if (dict.found("criticalShields"))
+    {
+        scalar Shc = readScalar(dict.lookup("criticalShields"));
+        critShields_.value() = Shc;
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::bedloadModel::~bedloadModel()
+{}
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+void Foam::bedloadModel::setCoefs
+(
+    scalar alpha,
+    scalar aExp,
+    scalar bExp
+)
+{
+    alpha_ = alpha;
+    aExp_ = aExp;
+    bExp_ = bExp;
+}
+
+void Foam::bedloadModel::setCoefs(const word& modelName)
 {
     if (modelName=="Meyer-Peter")
     {
         alpha_ = 8;
         aExp_ = 0;
-        bExp_ = 1.5;;
+        bExp_ = 1.5;
     }
     else if (modelName=="Nielsen")
     {
@@ -45,48 +131,12 @@ Foam::bedloadModel::bedloadModel
     }
     else
     {
-        Info << "not implemented yet" << endl;
+        FatalError << "bedloadModel " << modelName
+            << " for transport formula not a valide entry" << endl
+            << "possible names are: Meyer-Peter,  Nielsen" << endl;
+        Info << abort(FatalError) << endl;
     }
-
-    // default critical Shields number value
-    critShields_ = 0.047;
-
-    Info << "model " << modelName
-        << ", alpha: " << alpha_
-            << ", a: " << aExp_
-            << ", b: " << bExp_
-            << ", critShields: " << critShields_ << endl;
 }
-
-Foam::bedloadModel::bedloadModel
-(
-    const word& modelName,
-    scalar critShields
-)
-: bedloadModel(modelName)
-{
-    critShields_ = critShields;
-}
-
-Foam::bedloadModel::bedloadModel
-(
-    scalar alpha,
-    scalar a,
-    scalar b,
-    scalar critShields
-)
-:
-    alpha_(alpha),
-    aExp_(a),
-    bExp_(b),
-    critShields_(critShields)
-{}
-
-
-// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
-
-Foam::bedloadModel::~bedloadModel()
-{}
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
@@ -94,10 +144,10 @@ void Foam::bedloadModel::output(Ostream& os) const
 {
     os << "bedload model type: " << modelName_ << endl
         << "qb = alpha sqrt((s-1)gd^3) shields^a (shields-critShields)^b"
-        << endl << "alpha: " << alpha_
-        << ", a: " << aExp_
-        << ", b: " << bExp_
-        << ", critShields: " << critShields_ << endl;
+        << endl << "alpha: " << alpha_ << endl
+        << "a: " << aExp_ << endl
+        << "b: " << bExp_ << endl
+        << "critShields: " << critShields_.value() << endl;
 }
 
 Foam::Ostream& Foam::operator<<(Ostream& os, const bedloadModel& model)
