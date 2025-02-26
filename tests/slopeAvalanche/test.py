299fec2b0ef009caeca4422eb012d63669376558
Matthias.Renaud <renaud4ma@cl7n003.legi.grenoble-inp.fr>
2025-02-26 10:47:37 +0100

    add more verification for slopeAvalanche test

--- tests/slopeAvalanche/test.py
+++ tests/slopeAvalanche/test.py
@@ -7,13 +7,17 @@ from fluidfoam import readof as rdf
 
 print(" --- running slope avalanche --- ")
 
+success = True
+tolBetaRep = 1e-3
+tolQav = 1e-4
+
 time = "latestTime"
 
 dS = 0.5e-3  # sediment diameter
 rhoS = 2650.  # sediment density
 rhoF = 1000.  # fluid density
 g = 9.81  # gravity acceleration
-repAngle = 32  # repose angle of granular material (in rad)
+repAngle = 32.  # repose angle of granular material (in rad)
 
 # bed position
 Xbed, Ybed, Zbed = rdf.readmesh(
@@ -23,10 +27,34 @@ tanBed = np.abs((Zbed[2:] - Zbed[:-2]) / (Xbed[2:] - Xbed[:-2]))
 
 maxAngle = (180/np.pi) * np.max(np.arctan(tanBed))
 
-success = True
-tol = 1e-3
-relAngle = (maxAngle - repAngle) / repAngle
-if relAngle > tol:
+relAngleError = (maxAngle - repAngle) / repAngle
+if relAngleError > tolBetaRep:
     success = False
+    print(
+        f"error! relative difference between repose angle "
+        + f"({repAngle} deg) and maximum slope angle ({maxAngle} deg) "
+        + f"is {relAngleError*100} %\ntolerance is {tolBetaRep*100} %")
+else:
+    print("bed slope OK")
+
+# test value of avalanche
+Qav = 5e-3
+betaRep = repAngle * np.pi / 180
+qbX, qbY, qbZ = rdf.readvector(
+    "./", "0.1", "qbVf", boundary="bed", verbose=False)
+magQb = np.sqrt(qbX**2 + qbZ**2)
+beta = np.arctan(np.abs(qbZ/qbX))
+qavVinent = Qav * (np.tanh(np.tan(beta)) - np.tanh(np.tan(betaRep)))
+qavVinent /= (1 - np.tanh(np.tan(betaRep)))
+
+relErrQav = np.max(np.abs(magQb - qavVinent) / np.max(qavVinent))
+if relErrQav > tolQav:
+    success = False
+    print(
+        f"error! relative error on avalanche bedload flux value: "
+        + f"{relErrQav*100} %\ntolerance is {tolQav*100} %")
+else:
+    print("avalanche flux OK")
+
 
 assert success
